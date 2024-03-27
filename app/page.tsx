1698e9eecbd710a9c6f109b9a0dfbf7d91befdf6
HVMS <viralsid2330@gmail.com>
2024-03-27 03:15:07 -0300

    Login-Button.tsx component added and used for re-usability purpose for corresponding children elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
 import { Button } from "@/components/ui/button"
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  weight: ["600"],
+  subsets: ["latin"],
+})
 
 export default function Home() {
   return (
-    <Button size={"lg"} >Click me</Button>
+    <main className="flex h-full flex-col items-center justify-center
+      bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-yellow-300 to-green-300">
+      <div className="space-y-6 text-center">
+        <h1 className={cn(
+          "text-6xl font-semibold text-black drop-shadow-md",
+          font.className,
+        )}>
+          🔐 Auth
+        </h1>
+        <p className="text-black text-lg">
+          A Simple Authentication Service
+        </p>
+        <LoginButton>
+          <Button variant={"default"} size={"lg"}>
+            Sign In
+          </Button>
+        </LoginButton>
+      </div>
+    </main>
   );
 }
