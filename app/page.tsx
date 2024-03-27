ac7368010afd802fca50eaf865a8756d7c4549c5
HVMS <viralsid2330@gmail.com>
2024-03-27 02:37:56 -0300

    Button component using shadcn - tried to see what comes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <p className="font-semibold">Hello Viral Auth!!</p>
+    <Button size={"lg"} >Click me</Button>
   );
 }
