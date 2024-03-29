d7a23bef6004f5a41b834600ad1c415e5163a2ea
HVMS <viralsid2330@gmail.com>
2024-03-29 03:23:11 -0300

    Register component created

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/components/auth/login-form";
+import { RegisterForm } from "@/components/auth/register-form";
+
+export default function RegisterPage() {
+
+    return (
+        <RegisterForm />
+    );
+
+};
