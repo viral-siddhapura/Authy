f20dfd44bdee9001ef6ff439bdeae53b607660ba
HVMS <viralsid2330@gmail.com>
2024-03-29 17:26:34 -0300

    Register Server Action

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
 
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+
 export const register = async (values : z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
 
@@ -9,5 +12,31 @@ export const register = async (values : z.infer<typeof RegisterSchema>) => {
         return { error: "Invalid fields!!" };
     }
 
-    return { success: "Email sent!!"};
+    const { email, password, name } = validatedFields.data;
+    // Hash the password first
+    const hasedPassword = await bcrypt.hash(password, 10);
+
+    // Get the existing user
+    const existingUser = await db.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if (existingUser) {
+        return { error: "Email already exists!!" };
+    }
+
+    // Create the user
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hasedPassword,
+        },
+    });
+
+    // TODO: send verification email to the user
+
+    return { success: "Congratulations, User Created!"};
 }
