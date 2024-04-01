e4147eb3f5455f763fcb72873a1bc614d5f76ab5
HVMS <viralsid2330@gmail.com>
2024-03-31 23:53:22 -0300

    Middleware introduced - for logging and routing identification purpose

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  // req.auth
+  console.log("Route is : "+req.nextUrl.pathname);
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+}
