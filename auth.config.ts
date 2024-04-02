74bd4bb167d8a8d804588a8151e7d4e15174c06c
HVMS <viralsid2330@gmail.com>
2024-04-02 15:40:04 -0300

    Update authentication configuration

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,35 @@
-import GitHub from "next-auth/providers/github";
+import Credentials  from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 
+import bcrypt from "bcryptjs";
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+
 export default {
-    providers: [GitHub],
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginSchema.safeParse(credentials);
+
+                if (validatedFields.success) {
+                    const { email, password } = validatedFields.data;
+
+                    const user = await getUserByEmail(email);
+                    if (!user || !user.password) {
+                        return null;
+                    }
+
+                    const passwordMatch = await bcrypt.compare(
+                        password, 
+                        user.password
+                    );
+
+                    if (passwordMatch) {
+                        return user;
+                    }
+                }
+                return null;
+            }
+        }),
+    ],
 } satisfies NextAuthConfig;
