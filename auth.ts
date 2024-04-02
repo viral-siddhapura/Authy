f092dbcd2e39c5c1f82faf7df90a0a1f3f85c332
HVMS <viralsid2330@gmail.com>
2024-04-02 15:40:25 -0300

    Remove GitHub provider and add signIn and signOut functions

--- auth.ts
+++ auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
 import authConfig from './auth.config';
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from './lib/db';
@@ -7,6 +6,8 @@ import { db } from './lib/db';
 export const {
     handlers: {GET, POST},
     auth,
+    signIn,
+    signOut,
 } = NextAuth({
     adapter: PrismaAdapter(db),
     session: { strategy: 'jwt' },
