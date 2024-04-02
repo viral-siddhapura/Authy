fabb6ba2bf34dafab8e31d79af0724af72b9fe12
HVMS <viralsid2330@gmail.com>
2024-04-02 15:41:01 -0300

    Add bcryptjs dependency and signin flow deciced based on user actions

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,40 @@
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+  authRoutes,
+  apiAuthPrefix
+} from "./routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  // req.auth
-  console.log("Route is : "+req.nextUrl.pathname);
+  
+  const { nextUrl } = req;
+  const loggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRuote = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (loggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!loggedIn && !isPublicRuote) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return null;
+
 });
 
 // Optionally, don't invoke Middleware on some paths
