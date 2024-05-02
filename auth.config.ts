7cd4ec59f639a6ef5be5ba08c1acf87fe90fca1c
HVMS <viralsid2330@gmail.com>
2024-05-02 03:07:13 -0300

    Add Google and GitHub authentication providers in auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,27 @@
 import Credentials  from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 import bcrypt from "bcryptjs";
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 
+/***
+ *  This is the new auth.config.ts file. It's used for Authentication providers configuration.
+ *  Providers are used to authenticate users with different services like Google, Facebook, Github, etc.
+ */
+
 export default {
     providers: [
+        Google({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        Github({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
         Credentials({
             async authorize(credentials) {
                 const validatedFields = LoginSchema.safeParse(credentials);
