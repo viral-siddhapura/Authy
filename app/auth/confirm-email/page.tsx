0cfa95c963353af1ad9d9d1f69695d2febfa45b4
HVMS <viralsid2330@gmail.com>
2024-05-03 15:27:30 -0300

    feat: Add layout component for email confirmation page

---
+++ app/auth/confirm-email/page.tsx
@@ -0,0 +1,9 @@
+import { NewConfirmEmailForm } from "@/components/auth/new-confirm-email-form";
+
+const NewConfirmEmailPage = () => {
+    return (
+        <NewConfirmEmailForm/>
+    );
+}
+
+export default NewConfirmEmailPage;
