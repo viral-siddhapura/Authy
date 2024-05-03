2344dafbb80fb82cb572a6ea3d89c51128805a25
HVMS <viralsid2330@gmail.com>
2024-05-03 01:54:37 -0300

    feat: Add sendVerificationEmail function for email verification

---
+++ lib/mail.ts
@@ -0,0 +1,18 @@
+import {Resend} from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendVerificationEmail = async (
+    email: string,
+    token: string,
+) => {
+    const confirmedLink = `http://localhost:3000/auth/confirm-email?token=${token}`;
+
+    await resend.emails.send({
+        to: email,
+        from: "onboarding@resend.dev",
+        subject: "Confirm your email address",
+        html: `<p>Click the link below to confirm your email address:</p><a href="${confirmedLink}">Confirm your email</a>`,
+    });
+
+}
