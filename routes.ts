591f36f508882066b794b9f914b919aeabb400fe
HVMS <viralsid2330@gmail.com>
2024-05-04 01:24:01 -0300

    feat: Add reset password routes for password recovery

--- routes.ts
+++ routes.ts
@@ -7,6 +7,8 @@ export const authRoutes = [
     "/auth/login",
     "/auth/register",
     "/auth/error",
+    "/auth/reset",
+    "/auth/reset-password",
 ];
 
 export const apiAuthPrefix = "/api/auth";
